13b24676346dcbb4ed8f76134c7e7cb631adb178
Michael Girma <michaelgirma@Michaels-MacBook-Pro-71.local>
2025-05-15 20:10:17 -0400

    Adding files to new directory

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import playersRouter from './routes/players.js';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+app.use('/api/players', playersRouter);
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-}); 
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
